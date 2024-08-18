3de947142daef6aad5f8f633af6a0ab3937ccea4
Premlakshay Godara <89251510+Premlak@users.noreply.github.com>
2024-08-18 21:36:24 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import Editor from "@/components/editor"
+import Loading from "@/components/loading"
+import { LayerStore } from "@/lib/layer-store"
+import { ImageStore } from "@/lib/store"
+export default function Home() {
+  return (
+    <>
+    <div className="flex justify-center items-center mt-2">
+    <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+    <span class="font-medium">Warning alert!</span> Godara&apos;s Projects are into testing mode so you might face some issues 
+    </div>
+    </div>
+    <ImageStore.Provider
+      initialValue={{
+        activeTag: "all",
+        activeColor: "green",
+        activeImage: "",
+      }}
+    >
+      <LayerStore.Provider
+        initialValue={{
+          layerComparisonMode: false,
+          layers: [
+            {
+              id: crypto.randomUUID(),
+              url: "",
+              height: 0,
+              width: 0,
+              publicId: "",
+            },
+          ],
+        }}
+      >
+        <Editor />
+      </LayerStore.Provider>
+    </ImageStore.Provider>
+    </>
+  )
+}
