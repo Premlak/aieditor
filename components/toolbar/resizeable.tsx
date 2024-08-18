081cbf1505c0d9b678a788fb55b91ab494e8cd0e
Premlakshay Godara <89251510+Premlak@users.noreply.github.com>
2024-08-18 21:50:30 +0530

    Update resizeable.tsx

--- components/toolbar/resizeable.tsx
+++ components/toolbar/resizeable.tsx
@@ -5,8 +5,8 @@ import { motion, useMotionValue, useTransform } from "framer-motion"
 import { useImageStore } from "@/lib/store"
 
 export default function Resizable() {
-  const x = useMotionValue(0)
-  const y = useMotionValue(0)
+  const x: any = useMotionValue(0)
+  const y: any = useMotionValue(0)
   const setImageHeight = useImageStore((state) => state.setImageHeight)
   const setImageWidth = useImageStore((state) => state.setImageWidth)
 
