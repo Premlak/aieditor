fbb69047a4b890fab9bb60a06ed3963833a82c0f
Premlakshay Godara <89251510+Premlak@users.noreply.github.com>
2024-08-18 21:46:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
     <div className="flex justify-center items-center mt-2">
-    <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
-    <span class="font-medium">Warning alert!</span> Godara&apos;s Projects are into testing mode so you might face some issues 
+    <div className="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300" role="alert">
+    <span className="font-medium">Warning alert!</span> Godara&apos;s Projects are into testing mode so you might face some issues 
     </div>
     </div>
     <ImageStore.Provider
